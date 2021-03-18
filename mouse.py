95a957b653efc394bec5e08f74a17f1d3663def8
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-03-18 20:21:42 +0300

    Create mouse.py

---
+++ mouse.py
@@ -0,0 +1,11 @@
+int{
+  mouse( x + y )
+     d( mouse.controller)
+ }
+    int{
+       control.mouse(impostor.controller)
+      mouse( goToLocation = allLocation)
+    }
+       int{
+        control( Location + control)
+        }
